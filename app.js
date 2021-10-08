462c15d9817bd07a59d18db18d172f591fbe533f
CuzImBisonratte <CuzImBisonratte@gmail.com>
2021-10-08 21:47:22 +0200

    1.0.0

--- app.js
+++ app.js
@@ -1,24 +1,67 @@
-var fs = require('fs');
+// Database config
+var host = "localhost";
+var user = "root";
+var password = "";
+var database = "mini_projects";
+var table = "wifi_extractor"
+// 
+// 
+// Import the mysql-driver
 var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: ""
-});
-
+// Create the connection to the Database
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'mini_project'
+  host     : host,
+  user     : user,
+  password : password,
+  database : database
 });
- 
+// Import the xml-reader
+const xml2js = require('xml2js');
+// Import file-system functions
+const fs = require('fs');
+// Create a parser
+const parser = new xml2js.Parser({ attrkey: "ATTR" });
+// Let the system know, which file to use
+
+const testFolder = '../XMLs/';
 connection.connect();
- 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
+fs.readdir(testFolder, (err, files) => {
+  files.forEach(file => {
+    let xml_string = fs.readFileSync("../XMLs/"+file, "utf8");
+
+    parser.parseString(xml_string, function(error, result) {
+      if(error === null) {
+        console.log(result.WLANProfile);
+        wifi_name = result.WLANProfile.name[0];
+        wifi_encrypt = result.WLANProfile.MSM[0].security[0].authEncryption[0].encryption[0];
+        if(wifi_encrypt!="none"){
+          wifi_pass = result.WLANProfile.MSM[0].security[0].sharedKey[0].keyMaterial[0];
+        }
+        else{
+          wifi_pass = "-";
+        }
+        wifi_auth = result.WLANProfile.MSM[0].security[0].authEncryption[0].authentication[0];
+        console.log("WiFi name: "+wifi_name);
+        console.log("WiFi password: "+wifi_pass);
+        console.log("WiFi encryption: "+wifi_encrypt);
+        console.log("WiFi authentication: "+wifi_auth);
+        sql_statment = "INSERT INTO "+table+" (`name`, `password`, `encryption`, `authentication`) VALUES "
+        sql_statment = sql_statment + "('"+wifi_name+"','"+wifi_pass+"','"+wifi_encrypt+"','"+wifi_auth+"'"+")";
+        
+        connection.query(sql_statment, function (error, results, fields) {
+            if (error) throw error;
+        });
+        
+        
+      }
+      else {
+          console.log(error);
+      }
+    });
+  });
 });
+// connection.end();
+
  
-connection.end();
+
+ 
